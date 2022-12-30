f2b3f1a24b8fa20b5b1f537c027987261d16ab79
Ankit Gandhi <ankitgandhi452@gmail.com>
2022-12-30 17:01:04 +0530

    Modified: Function refenecing changes

--- src/JoseCrypto.mjs
+++ src/JoseCrypto.mjs
@@ -1,14 +1,11 @@
 import RSA from './lib/Rsa.mjs'
 import Aes from './lib/Aes.mjs'
 
-const { decrypt: decryptKey, encrypt: encryptKey } = RSA
-const { decrypt: decryptData, encrypt: encryptData } = Aes
-
 const JoseCrypto = {
-  decryptKey,
-  encryptKey,
-  encryptData,
-  decryptData
+  decryptKey: RSA.decrypt,
+  encryptKey: RSA.encrypt,
+  encryptData: Aes.encrypt,
+  decryptData: Aes.decrypt
 }
 
 export default JoseCrypto
