7be41db4c059cf2f103c0923f7998c93d489a3cc
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-06-28 18:08:40 +0530

    Fix JoseCryptoError

--- src/JoseCryptoError.ts
+++ src/JoseCryptoError.ts
@@ -6,7 +6,7 @@ const DEFAULT_ERROR_MSG = 'Jose Crypto Error'
 /** @ignore */
 const DEFAULT_ERROR_STATUS_CODE = 500
 /** @ignore */
-const DEFAULT_ERROR_CODE = 'JOSE_CRYPTO_ERROR'
+const DEFAULT_ERROR_CODE = 'JoseCrypto::GENERIC'
 
 /**
  * Error class whose instance is thrown in case of any error.
@@ -48,7 +48,7 @@ export default class JoseCryptoError extends Error {
    * Creates an instance of JoseCryptoError.
    *
    * @constructor
-   * @param [e] DOMException instance to wrap with JoseCryptoError.
+   * @param [e] Any Error instance to wrap with JoseCryptoError.
    * @param [eMap] JoseCryptoErrorMap to rewrap error for better understanding.
    */
   constructor(e: any, eMap: JoseCryptoErrorMap) {
